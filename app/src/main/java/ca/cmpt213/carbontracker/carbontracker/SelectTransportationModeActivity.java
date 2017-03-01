98200c6db97edb24710e82a4957fb0f15f8131e7
Khang Bui <khang.bui@outlook.com>
2017-02-28 22:58:26 -0800

    Create MainMenuActivity, SelectTransportationModeActivity, SelectRouteActivity and respective matching layouts. Set theme to HoloLight

---
+++ app/src/main/java/ca/cmpt213/carbontracker/carbontracker/SelectTransportationModeActivity.java
@@ -0,0 +1,15 @@
+package ca.cmpt213.carbontracker.carbontracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class SelectTransportationModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_transportation_mode);
+        Spinner selectCarSpinner = (Spinner) findViewById(R.id.select_car_spinner);
+    }
+}
