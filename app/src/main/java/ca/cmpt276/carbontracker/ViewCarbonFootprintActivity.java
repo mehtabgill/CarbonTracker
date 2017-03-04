d5bf0807828cfa5b11d0b15e75f4f891678e80b0
Elvin <elvinw@sfu.ca>
2017-03-04 13:18:22 -0800

    Fixed inconsistent package name

---
+++ app/src/main/java/ca/cmpt276/carbontracker/ViewCarbonFootprintActivity.java
@@ -0,0 +1,13 @@
+package ca.cmpt276.carbontracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewCarbonFootprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_carbon_footprint);
+    }
+}
